2c6bf1d3dc7c9bb7ab1cd4ff37419e0347596353
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-08-24 17:13:02 +0300

    Fixed graphql price field missing

--- frontend/components/Product.tsx
+++ frontend/components/Product.tsx
@@ -3,14 +3,18 @@ import Link from 'next/link';
 import { ProductTypes } from './Types';
 import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
+import PriceTag from './styles/PriceTagStyle';
 
 const Product = ({ product }) => {
+  console.log('============ ');
+  console.log(product.price);
   return (
     <ItemStyles>
       <img src={product.photo.image.publicUrlTransformed} alt={product.name} />
       <Title>
         <Link href={`product/${product.id}`}>{product.name}</Link>
       </Title>
+      <PriceTag>{product?.price}</PriceTag>
     </ItemStyles>
   );
 };
