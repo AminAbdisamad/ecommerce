4bb76cf025c1c0d401ef7f1cefa8711600c242ca
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-08-25 10:00:24 +0300

    added

---
+++ frontend/lib/graphql.ts
@@ -0,0 +1,19 @@
+/***  Create Mutations
+mutation{
+    createProduct(data:{
+      name:"Sample data",
+      description:"New description",
+      price:200,
+      status:"UNAVAILABLE"
+
+
+    }){
+      id,
+      name
+    }
+  }
+
+
+  **/
+
+export const File = () => 'nothing';
