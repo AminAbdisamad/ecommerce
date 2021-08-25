7c64ae84f6f80fcec015572629e6bbeda8d036dd
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-08-25 16:23:29 +0300

    update

--- frontend/components/UpdateProduct.tsx
+++ frontend/components/UpdateProduct.tsx
@@ -1,3 +1,122 @@
-export default function UpdateProduct() {
-  return <h1>Product Update</h1>;
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import DisplayError from '../lib/DisplayError';
+import useForm from '../lib/useForm';
+import { ProductDataTypes } from '../pages/product/[id]';
+import Product from './Product';
+import Form from './styles/Form';
+
+const GET_SINGLE_PRODUCT = gql`
+  query GET_SINGLE_PRODUCT($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      price
+      description
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      # DONOT Pass ID along
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  // We get a product by id and return product information from graphql
+  const { data, loading, error } = useQuery<ProductDataTypes>(
+    GET_SINGLE_PRODUCT,
+    { variables: { id } }
+  );
+
+  // We sent Updated data to the our graphql api and mutate it
+  const [
+    updateProduct,
+    { data: updateData, loading: updateLoading, error: updateError },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION);
+
+  const { inputs, handleChange } = useForm(
+    data?.Product || {
+      name: '',
+      price: '',
+      description: '',
+    }
+  );
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error || updateError) {
+    return <DisplayError error={error}></DisplayError>;
+  }
+
+  return (
+    <div>
+      <Form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const res = await updateProduct({
+            variables: {
+              id: id,
+              name: inputs.name,
+              description: inputs.description,
+              price: inputs.price,
+            },
+          });
+          console.log(res);
+        }}
+      >
+        <DisplayError error={error} />
+        <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+          <label htmlFor="name">
+            Name:
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              value={inputs.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="price">
+            Price:
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              value={inputs.price}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            <textarea
+              name="description"
+              id="description"
+              value={inputs.description}
+              onChange={handleChange}
+            ></textarea>
+          </label>
+          <button type="submit">Update Form</button>
+        </fieldset>
+      </Form>
+    </div>
+  );
 }
