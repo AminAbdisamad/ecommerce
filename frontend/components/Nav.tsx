8d53e8c589110a73e2884e6569ca5a38880da1fc
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-08-27 19:37:07 +0300

    update

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -1,15 +1,34 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import SignOut from './SignOut';
 import NavStyles from './styles/NavStyles';
+import useUser from './User';
 
 export default function Nav() {
+  const user = useUser();
+
   return (
     <NavStyles>
-      <Link href="/products">Product</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account </Link>
-      <Link href="/contacts">Contact</Link>
+      {user && (
+        <>
+          <Link href="/">
+            <a>Welcome Back, {user.name}!</a>
+          </Link>
+          <Link href="/products">Product</Link>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account </Link>
+          <SignOut />
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/products">Product</Link>
+          <Link href="/contacts">Contact</Link>
+          <Link href="/signin">Sign In</Link>
+          <Link href="/signup">Create Account</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
