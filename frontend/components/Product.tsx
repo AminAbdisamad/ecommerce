7293f543fabd43d9b45bed47b3d31924bed4ce46
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-08-30 22:10:24 +0300

    update

--- frontend/components/Product.tsx
+++ frontend/components/Product.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
-
+import styled from 'styled-components';
 import { ProductTypes } from './Types';
 import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
@@ -8,7 +8,16 @@ import PriceTag from './styles/PriceTagStyle';
 import formatMoney from '../lib/formatMoney';
 import DeleteProduct from './DeleteProduct';
 import useUser from './User';
+import AddToCart from './AddToCart';
 
+const ButtonSyles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid var(--secondaryColor);
+  border-bottom: 1px solid var(--secondaryColor);
+  padding: 1rem;
+`;
 const Product: React.FC<{ product: ProductTypes }> = ({ product }) => {
   const user = useUser();
   return (
@@ -24,7 +33,7 @@ const Product: React.FC<{ product: ProductTypes }> = ({ product }) => {
       <p>{product.description}</p>
       {/* btn  */}
       {user && (
-        <>
+        <ButtonSyles>
           <Link
             href={{
               pathname: 'products/update',
@@ -33,10 +42,11 @@ const Product: React.FC<{ product: ProductTypes }> = ({ product }) => {
               },
             }}
           >
-            <button>Edit</button>
+            <button type="button">Edit Product</button>
           </Link>
+          <AddToCart productId={product.id} />
           <DeleteProduct id={product.id} />
-        </>
+        </ButtonSyles>
       )}
     </ItemStyles>
   );
