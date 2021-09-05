ad04526438f09a4078ce62f69fa92466ec46b626
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-09-05 16:57:51 +0300

    Added Orders

--- backend/schemas/Order.ts
+++ backend/schemas/Order.ts
@@ -1,8 +1,17 @@
-import { integer, text, relationship } from '@keystone-next/fields';
+import { integer, text, relationship, virtual } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 
 export const Order = list({
   fields: {
+    label: virtual({
+      // virtual field is calculated on demand
+      // its not stored in the database
+
+      graphQLReturnType: 'String',
+      resolver(item) {
+        return item.total;
+      },
+    }),
     total: integer(),
     items: relationship({ ref: 'OrderItem.order', many: true }),
     user: relationship({
