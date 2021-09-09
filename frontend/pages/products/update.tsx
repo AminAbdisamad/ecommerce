c030f711eee228820d73918075718fbf7fbf8be0
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-09-09 19:00:46 +0300

    Update :tada:

--- frontend/pages/products/update.tsx
+++ frontend/pages/products/update.tsx
@@ -1,4 +1,8 @@
 import UpdateProduct from '../../components/UpdateProduct';
+import { x } from './index';
+
 export default function UpdatePage({ query }) {
   return <UpdateProduct id={query.id} />;
 }
+const y = x();
+console.log(y);
