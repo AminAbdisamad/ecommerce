83154e9997d6366e17ad7d1c6e8766d56054d083
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-09-25 02:57:17 +0300

    added all permisions

--- backend/access.ts
+++ backend/access.ts
@@ -6,6 +6,11 @@ export const isSignedIn = ({ session }: ListAccessArgs): boolean => !!session;
 export const generatePermissions = Object.fromEntries(
   permissionsList.map((permission) => [
     permission,
-    ({ session }: ListAccessArgs) => session?.data?.role?.[permission],
+    ({ session }: ListAccessArgs) => !!session?.data?.role?.[permission],
   ])
 );
+
+export const permissions = {
+  ...generatePermissions,
+  //   add extra permisions
+};
